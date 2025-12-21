8b526890424d0007bdcbeae352b56c402b578637
Deep07wk <karmakardeep279@gmail.com>
2025-12-21 16:44:59 +0530

    It generates random password of 15 digits

---
+++ script.js
@@ -0,0 +1,17 @@
+let char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function gen(){
+    document.getElementById("pass1").innerText=""
+    document.getElementById("pass2").innerText=""
+    for(let i=0;i<15;i++){
+        let random=Math.floor(Math.random()*91)
+        let pass=document.getElementById("pass1")
+        pass.innerText+=char[random]
+    }
+    for(let i=0;i<15;i++){
+        let random=Math.floor(Math.random()*91)
+        let pass=document.getElementById("pass2")
+        pass.innerText+=char[random]
+    }
+}
